8b46cbce2fbebc227be023a487cd0d078f609dfe
MihailKisluhin <89089003390@mail.ru>
2023-01-05 11:18:42 +0400

    05012023

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Kisluhin Mihail on 05.01.23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
